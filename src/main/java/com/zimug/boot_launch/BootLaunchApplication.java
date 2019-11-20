19d9cee6a76f92a78db8753da3b3e7feda8b35c4
dijianqiang <dijianqiang@vipkid.com.cn>
2019-11-20 19:03:47 +0800

    Initial commit

---
+++ src/main/java/com/zimug/boot_launch/BootLaunchApplication.java
@@ -0,0 +1,13 @@
+package com.zimug.boot_launch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootLaunchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootLaunchApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/zimug/boot_launch/model/Article.java
@@ -0,0 +1,21 @@
+package com.zimug.boot_launch.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+
+@Data
+@Slf4j
+@Builder
+@AllArgsConstructor
+public class Article {
+
+    private Long id;
+    private String author;
+    private String title;
+    private String content;
+    private String createTime;
+
+
+}
