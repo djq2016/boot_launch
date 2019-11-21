8e59d60f244af20f3d0143866171a512d757ccfb
djq2016 <1850285189@qq.com>
2019-11-21 08:45:27 +0800

    112修改springboot版本为2.1.5，lombok工具使用

--- src/main/java/com/zimug/boot_launch/controller/HelloController.java
+++ src/main/java/com/zimug/boot_launch/controller/HelloController.java
@@ -1,12 +1,30 @@
 package com.zimug.boot_launch.controller;
 
+import com.zimug.boot_launch.model.Article;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(String name) {
         return "hello world, " +name;
+    }
+
+
+
+    @RequestMapping("/testhello")
+    public Article testhello() {
+        /*Article article = new Article(1L,"zimug");
+
+        article.setAuthor("字母哥");*/
+
+        Article article1 = Article.builder().id(221L).author("莫言").build();
+
+        log.info("测试一下" + article1);
+
+        return article1;
     }
 }
--- src/main/java/com/zimug/boot_launch/model/Article.java
+++ src/main/java/com/zimug/boot_launch/model/Article.java
@@ -5,6 +5,8 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
+
+
 @Data
 @Slf4j
 @Builder
@@ -13,9 +15,7 @@ public class Article {
 
     private Long id;
     private String author;
-    private String title;
-    private String content;
-    private String createTime;
+
 
 
 }
