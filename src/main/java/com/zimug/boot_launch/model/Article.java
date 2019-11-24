ed5783d99bc0fbc6f5d4742c1a094182e79a40cd
djq2016 <1850285189@qq.com>
2019-11-24 22:40:48 +0800

    Mock 单元测试

--- src/main/java/com/zimug/boot_launch/model/Article.java
+++ src/main/java/com/zimug/boot_launch/model/Article.java
@@ -1,5 +1,8 @@
 package com.zimug.boot_launch.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,10 +18,17 @@ import java.util.List;
 @AllArgsConstructor
 public class Article {
 
+    @JsonIgnore
     private Long id;
+
+    //@JsonProperty("auther")
     private String author;
     private String title;
     private String content;
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
     private Date createTime;
     private List<Reader> reader;
+
 }
